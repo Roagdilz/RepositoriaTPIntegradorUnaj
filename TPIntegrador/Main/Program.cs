38c81ba1121aa9d5d91cf9879e9865ab8c07fafc
Roagdilz <diazlopezrodrigo14@gmail.com>
2023-04-15 18:03:38 -0300

    first commit

---
+++ TPIntegrador/Main/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Rodrigo
+ * Date: 15/4/2023
+ * Time: 17:55
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Main
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
