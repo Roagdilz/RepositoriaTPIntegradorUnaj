3b47e1be4a432551b908840bf739bfa292b15896
Roagdilz <diazlopezrodrigo14@gmail.com>
2023-04-26 00:40:08 +0200

    actualizando

--- TPIntegrador/Main/Obra.cs
+++ TPIntegrador/Main/Obra.cs
@@ -14,6 +14,7 @@ namespace Main
 	/// Description of Obra.
 	/// </summary>
 	public class Obra
+	//Comentario
 	{
 		// Variables de clase / estaticas / que siempre existiran;
 		private static int obras=0;
